fef5892019ea0dca22a5147ce82b268c0603c548
jiying-yun <56584080+jiying-yun@users.noreply.github.com>
2021-01-18 17:37:30 +0800

    Add files via upload

---
+++ test/src/main/java/Product.java
@@ -0,0 +1,40 @@
+import java.io.Serializable;
+
+public class Product implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    private Integer id;
+    private String name;
+
+
+    public Product(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Procuct{"+
+                "id = " + id +
+                ", name = " + name +'\''+
+               '}';
+    }
+}
